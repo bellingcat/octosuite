619070348bf854cdb7b500687b64589b7fec4b73
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-04-14 11:13:01 +0200

    Update banner.py

--- lib/banner.py
+++ lib/banner.py
@@ -1,5 +1,5 @@
 import os
-from lib.colors import red,white,green,reset
+from lib.colors import red,white,white_bg,red_bg,reset
 banner = f'''{red}
  ▒█████   ▄████▄  ▄▄▄█████▓ ▒█████    ██████  █    ██  ██▓▄▄▄█████▓▓█████ 
 ▒██▒  ██▒▒██▀ ▀█  ▓  ██▒ ▓▒▒██▒  ██▒▒██    ▒  ██  ▓██▒▓██▒▓  ██▒ ▓▒▓█   ▀ 
@@ -8,15 +8,16 @@ banner = f'''{red}
 ░ ████▓▒░▒ ▓███▀ ░  ▒██▒ ░ ░ ████▓▒░▒██████▒▒▒█████▓ ░██░  ▒██▒ ░ ░▒████▒
 ░ ▒░▒░▒░ ░ ░▒ ▒  ░  ▒ ░░   ░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░░▒▓▒ ▒ ▒ ░▓    ▒ ░░   ░░ ▒░ ░
   ░ ▒ ▒░   ░  ▒       ░      ░ ▒ ▒░ ░ ░▒  ░ ░░░▒░ ░ ░  ▒ ░    ░     ░ ░  ░
-░ ░ ░ ▒  ░          ░      ░ ░ ░ ▒  ░  ░  ░   ░░░ ░ ░  ▒ v1.5.1-beta
+░ ░ ░ ▒  ░          ░      ░ ░ ░ ▒  ░  ░  ░   ░░░ ░ ░  ▒ {red_bg}v1.5.2-alpha{reset}{red}
     ░ ░  ░ ░                   ░ ░        ░     ░      ░              ░  ░
          ░                              {white}— Advanced Github {red}OSINT{white} Framework{reset}
 
 
 
-> {white}Welcome {os.getlogin()}{reset}
-> {white}use '{green}help{white}' command for usage{reset}
-> {white}commands are case sensitive
+> {white}Current user: {white_bg}{os.getlogin()}{reset}
+> {white}Use {white_bg}help{reset}{white} command for usage{reset}
+> {white}Commands are {white_bg}case sensitive{reset}
 {'-'*30}
-{reset}
+
+
 '''

5718c5f229183ba33a7575a0786a9f4c1fd960f8
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-04-14 11:11:41 +0200

    Update colors.py

--- lib/colors.py
+++ lib/colors.py
@@ -1,16 +1,28 @@
+import os
 import sys
 
-# Colors will be unavailable on non-linux machines
 colors = True
-machine = sys.platform
+machine = sys.platform	
+# Colors will be unavailable on non-linux machines
 if machine.lower().startswith(("os", "win", "darwin","ios")): 
     colors = False
 
 if not colors:
-	reset = red = white = green  = ""
+	reset = red = white = green  = green_bg = white_bg = red_bg = ""
 
-else:                                                 
-    white = "\033[97m"
-    red = "\033[91m"    
-    reset = "\033[0m"
-    green = "\033[92m"
+else:
+    try:
+    	color_chooser = input(f"[ ? ] Welcome {os.getlogin()}, would you like to enable colors for this session? [Y/n] ")
+    	if color_chooser.lower() == "y":
+    		white = "\033[97m"
+    		white_bg = "\033[47;30m"
+    		red = "\033[91m"
+    		reset = "\033[0m"
+    		green = "\033[92m"
+    		green_bg = "\033[42;37m"
+    		red_bg = "\033[41;37m"
+    	else:
+    		red = white = green = green_bg = white_bg = red_bg = reset = ""
+    		
+    except KeyboardInterrupt:
+    	exit(f"[ ! ] Process interrupted with Ctrl+C")

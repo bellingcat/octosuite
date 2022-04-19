82b6a3fd3c1efaec3c81b247d30aeafae6a0196a
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-04-20 00:27:55 +0200

    Update colors.py

--- lib/colors.py
+++ lib/colors.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import platform
+from datetime import datetime
 
 colors = True
 machine = sys.platform	
@@ -12,7 +14,8 @@ if not colors:
 
 else:
     try:
-    	color_chooser = input(f"[ ? ] Welcome {os.getlogin()}, would you like to enable colors for this session? [Y/n] ")
+    	date_time = datetime.now()
+    	color_chooser = input(f"\t\t[OCTOSUITE] © 2022\n\t{date_time.strftime('%A %d %B %Y, %H:%M:%S%p')}\n\n\nOS: {platform.system()}\nProcessor: {platform.processor()}\nNode: {platform.node()}\nRelease: {platform.release()}\nArchitecture: {platform.architecture()}\nVersion: {platform.version()}\n\n[ ? ] Welcome {os.getlogin()}, would you like to enable colors for this session? [Y/n] ")
     	if color_chooser.lower() == "y":
     		white = "\033[97m"
     		white_bg = "\033[47;30m"

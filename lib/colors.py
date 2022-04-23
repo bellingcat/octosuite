1b5cfa2ebf3ef500c55211b993c8221485f9141a
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-04-23 13:17:37 +0200

    Update colors.py

--- lib/colors.py
+++ lib/colors.py
@@ -13,19 +13,25 @@ if not colors:
 	reset = red = white = green  = green_bg = white_bg = red_bg = ""
 
 else:
-    try:
-    	date_time = datetime.now()
-    	color_chooser = input(f"\t\t[OCTOSUITE] © 2022\n\t{date_time.strftime('%A %d %B %Y, %H:%M:%S%p')}\n\n\nOS: {platform.system()}\nProcessor: {platform.processor()}\nNode: {platform.node()}\nRelease: {platform.release()}\nArchitecture: {platform.architecture()}\nVersion: {platform.version()}\n\n[ ? ] Welcome {os.getlogin()}, would you like to enable colors for this session? [Y/n] ")
-    	if color_chooser.lower() == "y":
-    		white = "\033[97m"
-    		white_bg = "\033[47;30m"
-    		red = "\033[91m"
-    		reset = "\033[0m"
-    		green = "\033[92m"
-    		green_bg = "\033[42;37m"
-    		red_bg = "\033[41;37m"
-    	else:
-    		red = white = green = green_bg = white_bg = red_bg = reset = ""
-    		
-    except KeyboardInterrupt:
-    	exit(f"[ ! ] Process interrupted with Ctrl+C")
+    date_time = datetime.now()
+    print(f"\n\t OCTOSUITE © 2022 Richard Mwewa\n\t{date_time.strftime('%A %d %B %Y, %H:%M:%S%p')}\n\n\nOS: {platform.system()}\nProcessor: {platform.processor()}\nNode: {platform.node()}\nRelease: {platform.release()}\nArchitecture: {platform.architecture()}\nVersion: {platform.version()}\n\n")
+    while True:
+    	try:
+    		color_chooser = input(f"[ ? ] Welcome {os.getlogin()}, would you like to enable colors for this session? (y/n) ")
+    		if color_chooser.lower() == "y":
+    			white = "\033[97m"
+    			white_bg = "\033[47;30m"
+    			red = "\033[91m"
+    			reset = "\033[0m"
+    			green = "\033[92m"
+    			green_bg = "\033[42;37m"
+    			red_bg = "\033[41;37m"
+    			break
+    		elif color_chooser.lower() == "n":
+    		    red = white = green = green_bg = white_bg = red_bg = reset = ""
+    		    break
+    		else:
+    			print(f"\n[ ! ] Your response ({color_chooser}) is invalid (expected y or n)")
+    			
+    	except KeyboardInterrupt:
+    		exit(f"[ ! ] Process interrupted with Ctrl+C")

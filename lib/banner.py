be4e88d99bf205046f696c121ba8d67bcce5d675
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-04-27 15:29:53 +0200

    Update banner.py

--- lib/banner.py
+++ lib/banner.py
@@ -1,23 +1,23 @@
 import os
-from lib.colors import red,white,white_bg,red_bg,reset
+from lib.colors import red, white, green, red_bg,reset
 banner = f'''{red}
- ▒█████   ▄████▄  ▄▄▄█████▓ ▒█████    ██████  █    ██  ██▓▄▄▄█████▓▓█████ 
-▒██▒  ██▒▒██▀ ▀█  ▓  ██▒ ▓▒▒██▒  ██▒▒██    ▒  ██  ▓██▒▓██▒▓  ██▒ ▓▒▓█   ▀ 
-▒██░  ██▒▒▓█    ▄ ▒ ▓██░ ▒░▒██░  ██▒░ ▓██▄   ▓██  ▒██░▒██▒▒ ▓██░ ▒░▒███   
-▒██   ██░▒▓▓▄ ▄██▒░ ▓██▓ ░ ▒██   ██░  ▒   ██▒▓▓█  ░██░░██░░ ▓██▓ ░ ▒▓█  ▄ 
-░ ████▓▒░▒ ▓███▀ ░  ▒██▒ ░ ░ ████▓▒░▒██████▒▒▒█████▓ ░██░  ▒██▒ ░ ░▒████▒
-░ ▒░▒░▒░ ░ ░▒ ▒  ░  ▒ ░░   ░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░░▒▓▒ ▒ ▒ ░▓    ▒ ░░   ░░ ▒░ ░
-  ░ ▒ ▒░   ░  ▒       ░      ░ ▒ ▒░ ░ ░▒  ░ ░░░▒░ ░ ░  ▒ ░    ░     ░ ░  ░
-░ ░ ░ ▒  ░          ░      ░ ░ ░ ▒  ░  ░  ░   ░░░ ░ ░  ▒ {red_bg}v1.7.0-dev{reset}{red}
-    ░ ░  ░ ░                   ░ ░        ░     ░      ░              ░  ░
-         ░                              {white}— Advanced Github {red}OSINT{white} Framework{reset}
+       ▒█████   ▄████▄  ▄▄▄█████▓ ▒█████    ██████  █    ██  ██▓▄▄▄█████▓▓█████ 
+      ▒██▒  ██▒▒██▀ ▀█  ▓  ██▒ ▓▒▒██▒  ██▒▒██    ▒  ██  ▓██▒▓██▒▓  ██▒ ▓▒▓█   ▀ 
+      ▒██░  ██▒▒▓█    ▄ ▒ ▓██░ ▒░▒██░  ██▒░ ▓██▄   ▓██  ▒██░▒██▒▒ ▓██░ ▒░▒███   
+      ▒██   ██░▒▓▓▄ ▄██▒░ ▓██▓ ░ ▒██   ██░  ▒   ██▒▓▓█  ░██░░██░░ ▓██▓ ░ ▒▓█  ▄ 
+      ░ ████▓▒░▒ ▓███▀ ░  ▒██▒ ░ ░ ████▓▒░▒██████▒▒▒█████▓ ░██░  ▒██▒ ░ ░▒████▒
+      ░ ▒░▒░▒░ ░ ░▒ ▒  ░  ▒ ░░   ░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░░▒▓▒ ▒ ▒ ░▓    ▒ ░░   ░░ ▒░ ░
+        ░ ▒ ▒░   ░  ▒       ░      ░ ▒ ▒░ ░ ░▒  ░ ░░░▒░ ░ ░  ▒ ░    ░     ░ ░  ░
+      ░ ░ ░ ▒  ░          ░      ░ ░ ░ ▒  ░  ░  ░   ░░░ ░ ░  ▒ {red_bg} v1.8.0 {reset}{red}
+          ░ ░  ░ ░                   ░ ░        ░     ░      ░              ░  ░
+               ░                              {white}— Advanced Github {red}OSINT{white} Framework{reset}
 
 
 
-> {white}Current user: {white_bg}{os.getlogin()}{reset}
-> {white}Use {white_bg}help{reset}{white} command for usage{reset}
-> {white}Commands are {white_bg}case sensitive{reset}
-{'-'*30}
+<> {white}Current user: {green}{os.getlogin()}{reset}
+<> {white}Use {green}help{reset}{white} command for usage{reset}
+<> {white}Commands are case sensitive{reset}
+   {'-'*27}
 
 
 '''

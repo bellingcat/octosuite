2ab70c073744a37ca50b58adb8ee29db5bca11b7
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-06-05 14:36:18 +0200

    Update colors.py

--- utilities/colors.py
+++ utilities/colors.py
@@ -38,7 +38,7 @@ class Color:
 	    print("\n")
 	    while True:
 	        try:
-	            color_chooser = input(f"[ ? ] Welcome, would you like to enable colors for this session? (y/n) ")
+	            color_chooser = input(f"[ ? ] Would you like to enable colors for this session? (y/n) ")
 	            if color_chooser.lower() == "y":
 	                white = "\033[97m"
 	                red = "\033[91m"

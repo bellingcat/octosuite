e062065c753a32d9bcf7a8e4a6daba4cd0841a63
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-06-23 12:36:11 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="octosuite",
+    version="2.1.0",
+    author="Richard Mwewa",
+    author_email="richardmwewa@duck.com",
+    packages=["octosuite"],
+    description="Advanced Github OSINT Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rly0nheart/octosuite",
+    license="GNU General Public License v3 (GPLv3)",
+    install_requires=["requests"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',  
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3'
+        ],
+    entry_points={
+        "console_scripts": [
+            "octosuite=octosuite.main:onStart",
+        ]
+    },
+)

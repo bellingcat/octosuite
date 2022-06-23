33396149846cab95ee4e5f7ca5382c4f815841d1
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-06-23 12:24:22 +0200

    Create banner.py

---
+++ octosuite/banner.py
@@ -0,0 +1,23 @@
+import getpass
+from octosuite.colors import Color
+
+'''
+Banner
+This class holds the program's banner logo and version tag
+'''
+class Banner:
+    versionTag = '2.1.0'
+    nameLogo = f'''{Color.white}
+  _______        __          _______         __ __         
+ |       |.----.|  |_.-----.|     __|.--.--.|__|  |_.-----.
+ |   -   ||  __||   _|  _  ||__     ||  |  ||  |   _|  -__|
+ |_______||____||____|_____||_______||_____||__|____|_____|
+                                                     v{versionTag}
+                         {Color.white}— Advanced Github {Color.red}OSINT{Color.white} Framework{Color.reset}
+
+
+
+.:{getpass.getuser()}:.
+{Color.white}├─ use {Color.green}help{Color.reset}{Color.white} command for usage{Color.reset}
+{Color.white}└╼ commands are case insensitive{Color.reset}
+'''

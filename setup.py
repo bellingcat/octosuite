587641cd18748a8b15da0019ca5fb29b160fa2cd
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-06-26 10:07:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as file:
 
 setuptools.setup(
     name="octosuite",
-    version="2.1.0",
+    version="2.1.1",
     author="Richard Mwewa",
     author_email="richardmwewa@duck.com",
     packages=["octosuite"],

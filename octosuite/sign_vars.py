6a87cb51deb5e51552403c598f4036302dc6b08f
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-07-02 22:02:58 +0200

    Update sign_vars.py

--- octosuite/sign_vars.py
+++ octosuite/sign_vars.py
@@ -1,15 +1,15 @@
-from octosuite.colors import Color
+from octosuite.colors import  red, white, green, reset
 
 '''
-Attributes
+SignVar
 *Even here, I couldn't think of a good name.*
 The Attributes class holds the signs/symbols that show what a notification in OctoSuite might be all about.
 This might not be very important or necessary in some cases, but I think it's better to know the severerity of the notifications you get in a program.
 '''
 class SignVar:
-	prompt = f'{Color.white}[{Color.green} ? {Color.white}]{Color.reset}'
-	warning = f'{Color.white}[{Color.red} ! {Color.white}]{Color.reset}'
-	error = f'{Color.white}[{Color.red} x {Color.white}]{Color.reset}'
-	positive = f'{Color.white}[{Color.green} + {Color.white}]{Color.reset}'
-	negative = f'{Color.white}[{Color.red} - {Color.white}]{Color.reset}'
-	info = f'{Color.white}[{Color.green} * {Color.white}]{Color.reset}'
+	prompt = f"{white}[{green} ? {white}]{reset}"
+	warning = f"{white}[{red} ! {white}]{reset}"
+	error = f"{white}[{red} x {white}]{reset}"
+	positive = f"{white}[{green} + {white}]{reset}"
+	negative = f"{white}[{red} - {white}]{reset}"
+	info = f"{white}[{green} * {white}]{reset}"

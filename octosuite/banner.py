4dc2dced3297f20ceb23f12ee5b7b95bf74006ed
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-07-02 21:45:54 +0200

    Update banner.py

--- octosuite/banner.py
+++ octosuite/banner.py
@@ -1,23 +1,22 @@
 import getpass
-from octosuite.colors import Color
+from octosuite.colors import red, white, green, reset
 
 '''
-Banner
-This class holds the program's banner logo and version tag
+banner.py
+This file holds the program's banner logo and version tag
 '''
-class Banner:
-    versionTag = '2.1.1'
-    nameLogo = f'''{Color.white}
+version_tag = "2.2.0"
+name_logo = f"""{white}
   _______        __          _______         __ __         
  |       |.----.|  |_.-----.|     __|.--.--.|__|  |_.-----.
  |   -   ||  __||   _|  _  ||__     ||  |  ||  |   _|  -__|
  |_______||____||____|_____||_______||_____||__|____|_____|
-                                                     v{versionTag}
-                          {Color.white}— Advanced Github {Color.red}OSINT{Color.white} Framework{Color.reset}
+                                                     v{version_tag}
+                          {white}— Advanced Github {red}OSINT{white} Framework
 
 
 
 .:{getpass.getuser()}:.
-{Color.white}├─ use {Color.green}help{Color.reset}{Color.white} command for usage{Color.reset}
-{Color.white}└╼ commands are case insensitive{Color.reset}
-'''
+├─ use ‘{green}help{reset}{white}’ command for usage
+└╼ commands are case insensitive{reset}
+"""

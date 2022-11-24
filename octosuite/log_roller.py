26cf25bf91ea8a148e7c33c7a170fee767d2b665
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-11-25 01:48:55 +0200

    Update log_roller.py

--- octosuite/log_roller.py
+++ octosuite/log_roller.py
@@ -1,28 +1,25 @@
-"""
-logRoller This class is where the main notification strings/messages are held, and are being used in two different
-cases (they're being used by logging to be written to log files, and being printed out to the screen).
-"""
+# LogRoller This class is where the main notification strings/messages are held, and are being used in two different
+# cases (they're being used by logging to be written to log files, and being printed out to the screen).
 
 
-class logRoller:
-    Ctrl = "Session terminated with {}."
-    Error = "An error occurred: {}"
-    sessionOpened = "Opened new session on {}:{}"
-    sessionClosed = "Session closed at {}."
-    viewingLogs = "Viewing logs..."
-    viewingCsv = "Viewing CSV file(s)..."
-    deletedLog = "Deleted log -> {}"
-    readingLog = "Reading log -> {}"
-    readingCsv = 'Reading csv -> {}'
-    deletedCsv = 'Deleted csv -> {}'
-    fileDownloading = "Downloading -> {}..."
-    fileDownloaded = "Downloaded -> downloads/{}"
-    infoNotFound = "Information not found -> ({} - {} - {})"
-    repoNotFound = "Repository not found -> ({})"
-    userNotFound = "User not found -> (@{})"
-    orgNotFound = "Organization not found -> (@{})"
-    repoOrUserNotFound = "Repository or user not found -> ({} - @{})"
-    askLogCsv = "Do you wish to log this output to a .csv file? (Y/n) "
-    loggedToCsv = "Output logged -> ({})"
-    loggingSkipped = "Logging skipped -> ({})"
-    limitInput = "Limit '{}' output to how many? (1-100) "
+class LogRoller:
+    ctrl_c = "Session terminated with Ctrl+C."
+    error = "An error occurred: {}"
+    session_opened = "Opened new session on {}:{}"
+    session_closed = "Session closed at {}."
+    viewing_logs = "Viewing logs"
+    viewing_csv = "Viewing CSV file(s)..."
+    deleted_log = "Deleted log: {}"
+    reading_log = "Reading log: {}"
+    reading_csv = 'Reading csv: {}'
+    deleted_csv = 'Deleted csv: {}'
+    file_downloading = "Downloading: {}"
+    file_downloaded = "Downloaded: downloads/{}"
+    info_not_found = "Information not found: {}, {}, {}"
+    user_not_found = "User not found: @{}"
+    org_not_found = "Organization not found: @{}"
+    repo_or_user_not_found = "Repository or User not found: {}, @{}"
+    prompt_log_csv = "Do you wish to log this output to a CSV file? (yes/no) "
+    logged_to_csv = "Output logged: {}"
+    logging_skipped = "Logging skipped: {}"
+    limit_output = "Limit '{}' output to how many? (1-100) "

94c1e0a737a099a6de7ee55c07b74ac946e1e3c7
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-11-25 01:59:04 +0200

    Update and rename sign_vars.py to message_prefixes.py

--- octosuite/message_prefixes.py
+++ octosuite/message_prefixes.py
@@ -1,11 +1,11 @@
 from octosuite.colors import red, white, green, reset
 
 """
-SignVar *Even here, I couldn't think of a good name.* The Attributes class holds the signs/symbols that show what 
+MessagePrefix *Even here, I couldn't think of a good name.* The Attributes class holds the signs/symbols that show what 
 a notification in OctoSuite might be all about. This might not be very important or necessary in some cases, 
 but I think it's better to know the severity of the notifications you get in a program. 
 """
-class SignVar:
+class MessagePrefix:
     prompt = f"{white}[{green} ? {white}]{reset}"
     warning = f"{white}[{red} ! {white}]{reset}"
     error = f"{white}[{red} x {white}]{reset}"

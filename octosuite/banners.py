eb724f629f2d8b9e67126ab194c034943f8ea16a
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-11-25 02:33:59 +0200

    Update banners.py

--- octosuite/banners.py
+++ octosuite/banners.py
@@ -5,39 +5,35 @@ from octosuite.colors import red, white, green, reset
 
 # banners.py
 # This file holds the program's banners and version tag
-version_tag = "3.0.0"
+version_tag = "2.2.3"
 
 
 def ascii_banner():
-    banner_tree = Tree(getpass.getuser())
-    banner_tree.add(f"use ‘{green}help{reset}’ command for usage")
-    banner_tree.add(f"commands are case insensitive\n")
     ascii_banners = [
-        """
+"""
   _______        __          _______         __ __         
  |       |.----.|  |_.-----.|     __|.--.--.|__|  |_.-----.
  |   -   ||  __||   _|  _  ||__     ||  |  ||  |   _|  -__|
  |_______||____||____|_____||_______||_____||__|____|_____|
-                                                    """,
-        """
+                                                    """, 
+"""
 ╔═╗┌─┐┌┬┐┌─┐╔═╗┬ ┬┬┌┬┐┌─┐
 ║ ║│   │ │ │╚═╗│ ││ │ ├┤ 
 ╚═╝└─┘ ┴ └─┘╚═╝└─┘┴ ┴ └─┘
                       """,
-        """
+"""
 ░▒█▀▀▀█░█▀▄░▀█▀░▄▀▀▄░▒█▀▀▀█░█░▒█░░▀░░▀█▀░█▀▀
 ░▒█░░▒█░█░░░░█░░█░░█░░▀▀▀▄▄░█░▒█░░█▀░░█░░█▀▀
 ░▒█▄▄▄█░▀▀▀░░▀░░░▀▀░░▒█▄▄▄█░░▀▀▀░▀▀▀░░▀░░▀▀▀
                                          """,
-        """
+"""
  ▄▀▄ ▄▀▀ ▀█▀ ▄▀▄ ▄▀▀ █ █ █ ▀█▀ ██▀
  ▀▄▀ ▀▄▄  █  ▀▄▀ ▄██ ▀▄█ █  █  █▄▄
                               """]
     ascii_banner = random.choice(ascii_banners)
-    return banner_tree, f"""{ascii_banner} v{version_tag}
+    return f"""{ascii_banner} v{version_tag}
                           {white}— Advanced Github {red}OSINT{white} Framework
-
-
-
+.:{getpass.getuser()}:.
+├─ use ‘{green}help{reset}{white}’ command for usage
+└╼ commands are case insensitive{reset}
 """
-

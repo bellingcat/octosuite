2537bcbab84d37529481655a683121fb70b472fa
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-11-25 02:29:04 +0200

    Update message_prefixes.py

--- octosuite/message_prefixes.py
+++ octosuite/message_prefixes.py
@@ -6,9 +6,9 @@ a notification in OctoSuite might be all about. This might not be very important
 but I think it's better to know the severity of the notifications you get in a program. 
 """
 class MessagePrefix:
-    prompt = f"{white}[{green} ? {white}]{reset}"
-    warning = f"{white}[{red} ! {white}]{reset}"
-    error = f"{white}[{red} x {white}]{reset}"
-    positive = f"{white}[{green} + {white}]{reset}"
-    negative = f"{white}[{red} - {white}]{reset}"
-    info = f"{white}[{green} * {white}]{reset}"
+    prompt = f"{white}[{green}?{white}]{reset}"
+    warning = f"{white}[{red}!{white}]{reset}"
+    error = f"{white}[{red}x{white}]{reset}"
+    positive = f"{white}[{green}+{white}]{reset}"
+    negative = f"{white}[{red}-{white}]{reset}"
+    info = f"{white}[{green}*{white}]{reset}"

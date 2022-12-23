331c81ac46b93fff4d60f4c197ce9f798ae0acab
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-12-23 12:16:39 +0200

    Update and rename banners.py to banner.py

---
+++ octosuite/banner.py
@@ -0,0 +1,24 @@
+import getpass
+from rich.tree import Tree
+from octosuite.config import red, white, green, reset
+
+
+# banner.py
+# This file holds the program's banner and version tag
+version_tag = "3.0.0"
+
+
+def banner():
+    banner_tree = Tree(getpass.getuser())
+    banner_tree.add(f"use ‘{green}help{reset}’ command for usage")
+    banner_tree.add(f"commands are case insensitive\n")
+    return f"""
+  _______        __          _______         __ __         
+ |       |.----.|  |_.-----.|     __|.--.--.|__|  |_.-----.
+ |   -   ||  __||   _|  _  ||__     ||  |  ||  |   _|  -__|
+ |_______||____||____|_____||_______||_____||__|____|_____|
+                                                 v{version_tag}#dev
+                          {white}— Advanced Github {red}OSINT{white} Framework
+
+
+""", banner_tree

3e18a3301ae8ca2d8f0ccbb04696f69ff33cac5b
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-01-22 17:02:39 +0200

    Merge pull request from #9
     fix: bad indentation leading to reference before assignment error #9

--- octosuite/banner.py
+++ octosuite/banner.py
@@ -4,7 +4,7 @@ from octosuite.config import red, white, green, reset, Tree
 
 # banner.py
 # This file holds the program's banner and version tag
-version_tag = "3.0.1"
+version_tag = "3.0.4"
 
 
 def banner():
@@ -16,7 +16,7 @@ def banner():
  |       |.----.|  |_.-----.|     __|.--.--.|__|  |_.-----.
  |   -   ||  __||   _|  _  ||__     ||  |  ||  |   _|  -__|
  |_______||____||____|_____||_______||_____||__|____|_____|
-                                                 v{version_tag}#dev
+                                                 v{version_tag}
                           {white}— Advanced Github {red}OSINT{white} Framework
 
 
